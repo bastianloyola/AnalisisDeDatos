708bdb36f4f650fd09d742c2b017e5ff9aca9d6c
Bastián Loyola <bastian.loyola@usach.cl>
2022-09-17 20:45:04 -0300

    Correlacion y regresion logistica

--- lab1.R
+++ lab1.R
@@ -1,1 +1,30 @@
-columns = c("area", "perimeter", "compactness", "length", "width", "AC", "lengthGroove", "class")
+
+
+bank <- read.csv('https://raw.githubusercontent.com/bastianloyola/AnalisisDeDatos/main/bank/bank.csv', sep=';')
+
+#
+bank$y <- factor(bank$y)
+
+
+#Resumen base de datos
+summary(bank)
+
+
+cor(bank$duration,bank$pdays)
+
+
+cor(bank$duration,bank$age)
+
+#Regresion logistica entre duracion e Y
+logistica <- glm(y ~ duration, family = "binomial", data = bank)
+
+print(summary(logistica))
+
+#Exactitud
+sum(round(predict(logistica, type = "response")) == as.numeric(bank$y)) / length(bank$y)
+
+#Sensibilidad
+sum(round(predict(logistica, type = "response")) == as.numeric(bank$y) & as.numeric(bank$y) == 1) / sum(as.numeric(bank$y))
+
+#Presicion
+sum(round(predict(logistica, type = "response")) == as.numeric(bank$y) & as.numeric(bank$y) == 1) / sum(round(predict(logistica, type = "response") == 1))
